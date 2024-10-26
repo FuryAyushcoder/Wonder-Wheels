a4dc43fd614cfdba4d5540cf8df30b7bf2ef4ec5
Ayush Gumgaonkar <140783122+FuryAyushcoder@users.noreply.github.com>
2024-10-26 21:27:02 +0530

    Update index.ts

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,25 +1,29 @@
 import { Listing, Reservation, User } from "@prisma/client";
 
-export type SafeListing = Omit<Listing, "createdAt"> & {
+// SafeListing type - removes "createdAt" and redefines it as a string
+export type SafeListing = {
+  [K in keyof Omit<Listing, "createdAt">]: Listing[K];
+} & { createdAt: string };
+
+// SafeUser type - removes sensitive fields and redefines certain dates as strings
+export type SafeUser = {
+  [K in keyof Omit<User, "createdAt" | "updatedAt" | "emailVerified">]: User[K];
+} & {
   createdAt: string;
+  updatedAt: string;
+  emailVerified: string | null;
 };
 
-export type SafeReservation = Omit<
-  Reservation, 
-  "createdAt" | "startDate" | "endDate" | "listing" | "user"
-> & {
+// SafeReservation type - removes certain fields and redefines dates as strings
+export type SafeReservation = {
+  [K in keyof Omit<
+    Reservation,
+    "createdAt" | "startDate" | "endDate" | "listing" | "user"
+  >]: Reservation[K];
+} & {
   createdAt: string;
   startDate: string;
   endDate: string;
   listing: SafeListing;
   user: SafeUser;
-};
-
-export type SafeUser = Omit<
-  User,
-  "createdAt" | "updatedAt" | "emailVerified"
-> & {
-  createdAt: string;
-  updatedAt: string;
-  emailVerified: string | null;
 };
